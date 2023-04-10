4bc3443337ada4a1ac142a09ffc11e3356f787be
Jordana Clarimundo Dionisio <115229693+Jordana-Clarimundo@users.noreply.github.com>
2023-04-10 10:37:07 -0300

    função estrutura condicionada

---
+++ funcaoestruturacondicao.dart
@@ -0,0 +1,17 @@
+void main(){
+  jedi("Dieimes", "Gustavo");
+  jedi("Dieimes", "Alvaro");
+  jedi("Dieimes", "Ana Maria");
+  jedi("Dieimes", "VItoria", false);
+}
+
+void jedi(String professor, aluno, [bool dia = true]) {
+  print("o professor $professor é um mestre jedi");
+  print("o aluno $aluno é um padawan");
+
+  if(dia == true) {
+    print("bom dia");
+  } else {
+    print("boa tarde");
+  }
+}
