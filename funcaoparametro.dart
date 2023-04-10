d639e2e27751282dbed3fed22b720bc5a1d39fc8
Jordana Clarimundo Dionisio <115229693+Jordana-Clarimundo@users.noreply.github.com>
2023-04-10 10:27:39 -0300

    função com parâmetro

---
+++ funcaoparametro.dart
@@ -0,0 +1,11 @@
+void main(){
+  jedi("Dieimes", "Gustavo");
+  jedi("Dieimes", "Alvaro");
+  jedi("Dieimes", "Ana Maria");
+  jedi("Dieimes", "VItoria");
+}
+
+void jedi(String professor, aluno) {
+  print("o professor $professor é um mestre jedi");
+  print("o aluno $aluno é um padawan");
+}
